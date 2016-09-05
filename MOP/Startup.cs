903c37fe75bcabec59c14ea6e52dd2e2633387f2
waqasaslammeo <14030027033@umt.edu.pk>
2016-09-05 11:24:36 +0500

    First Commit

---
+++ MOP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MOP.Startup))]
+namespace MOP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
