dfb16c76c056f4e42a341d9d260ed8faee7b741b
Abdul Khaliq <hak3442@gmail.com>
2016-09-06 14:14:24 +0500

    Abdul Khaliq. i Have completed my model classes.

--- BusinessEntity/Context/SMSContext.cs
+++ BusinessEntity/Context/SMSContext.cs
@@ -13,6 +13,14 @@ namespace BusinessEntity.Context
         public DbSet<Section> Sections { get; set; }
         public DbSet<Class> Classes { get; set; }
         public DbSet<Grade> Grades { get; set; }
-
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SystemLog> SystemLogs { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<SystemItem> SystemItems { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoleItem> RoleItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
     }
 }
