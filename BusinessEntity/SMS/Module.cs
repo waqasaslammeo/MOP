6210f955228369c025be59bd1c3faf306280149d
Abdul Khaliq <hak3442@gmail.com>
2016-09-06 15:26:14 +0500

    Abdul Khaliq
    partial update, due to issues in kamran end.

---
+++ BusinessEntity/SMS/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessEntity.SMS
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public string ModuleName { get; set; }
+        public int SubjectId { get; set; }
+        public virtual Subject Subject { get; set; }
+
+
+
+    }
+}
---
+++ Repositories/PartialSmsRepository.cs
@@ -0,0 +1,57 @@
+﻿using BusinessEntity.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessEntity.SMS;
+
+namespace Repositories
+{
+    public static class PartialSmsRepository
+    {
+        #region Insert
+        public static void InsertRole(Role role)
+        {
+            using (SMSContext db = new SMSContext())
+            {
+                db.Roles.Add(role);
+                db.SaveChanges();
+            }
+        }
+        #endregion
+        #region Update
+        public static void UpdateRole(Role role)
+        {
+            using (SMSContext db = new SMSContext())
+            {
+                var objrole = db.Roles.Single(x => x.Id == role.Id);
+                objrole.RoleName = role.RoleName;
+                db.SaveChanges();
+            }
+        }
+        #endregion
+
+        #region Delete
+        public static void DeleteRole(int roleId)
+        {
+            using (SMSContext db = new SMSContext())
+            {
+                var tempRole = db.Roles.Single(x => x.Id == roleId);
+                db.Roles.Remove(tempRole);
+                db.SaveChanges();
+            }
+        }
+        #endregion
+
+        #region
+        #endregion
+
+
+    }
+
+
+
+
+
+
+}
