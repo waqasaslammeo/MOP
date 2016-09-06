aabb9df9eb14d1177fe5c3819fcde07717aed952
Usama Ikram <usamaikram901@gmail.com>
2016-09-06 15:59:47 +0500

    Usama Ikram I am getting new Code from Adbul Khaliq

--- BusinessEntity/Context/SMSContext.cs
+++ BusinessEntity/Context/SMSContext.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Web;
 using BusinessEntity.SMS;
+using BusinessEntity.Context;
 using System.Configuration;
 namespace BusinessEntity.Context
 {
@@ -13,14 +14,14 @@ namespace BusinessEntity.Context
         public DbSet<Section> Sections { get; set; }
         public DbSet<Class> Classes { get; set; }
         public DbSet<Grade> Grades { get; set; }
-        public DbSet<Session> Sessions { get; set; }
-        public DbSet<SystemLog> SystemLogs { get; set; }
-        public DbSet<Subject> Subjects { get; set; }
-        public DbSet<Module> Modules { get; set; }
-        public DbSet<SystemItem> SystemItems { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<RoleItem> RoleItems { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
+        //public DbSet<Session> Sessions { get; set; }
+        //public DbSet<SystemLog> SystemLogs { get; set; }
+        //public DbSet<Subject> Subjects { get; set; }
+        //public DbSet<Module> Modules { get; set; }
+        //public DbSet<SystemItem> SystemItems { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+        //public DbSet<RoleItem> RoleItems { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
     }
 }
