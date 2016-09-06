890bc78184482ad9781886b6d97ee2d8d0e31ee9
Abdul Khaliq <hak3442@gmail.com>
2016-09-06 12:29:29 +0500

    Abdul Khaliq New Classes in SMS has been added 1-Section 2-Classes 3-Grade

---
+++ BusinessEntity/Context/SMSContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Web;
+using BusinessEntity.SMS;
+using System.Configuration;
+namespace BusinessEntity.Context
+{
+    public class SMSContext :DbContext
+    {
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
