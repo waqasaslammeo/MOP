0ef293cfbefe130a8bbcfbb68f1a66b462d75805
Abdul Khaliq <hak3442@gmail.com>
2016-09-20 09:48:03 +0500

    Abdul Khaliq Adding files

---
+++ BusinessEntity/Context/IContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using BusinessEntity.SMS;
+
+namespace BusinessEntity.Context
+{
+    public interface IContext
+    {
+        DbSet<Role> Roles { get; set; }
+    }
+}
