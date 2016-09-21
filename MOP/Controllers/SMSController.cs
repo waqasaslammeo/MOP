9b7eeaec558217df9ed8b5d8e6e41b7df5c99da6
Usama Ikram <usamaikram901@gmail.com>
2016-09-21 10:06:15 +0500

    Usama Ikram ------------------------- Pushing code on server, after integration of front end design ,menu , and buttons.

---
+++ MOP/Controllers/SMSController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOP.Controllers
+{
+    public class SMSController : Controller
+    {
+        //
+        // GET: /SMS/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Branch()
+        {
+            return PartialView("Branch");
+        }
+
+        public PartialViewResult City()
+        {
+            return PartialView("City");
+        }
+
+        public PartialViewResult Grade()
+        {
+            return PartialView("Grade");
+        }
+
+        public PartialViewResult Class()
+        {
+            return PartialView("Class");
+        }
+
+        public PartialViewResult Module()
+        {
+            return PartialView("Module");
+        }
+
+        public PartialViewResult PreviousSchool()
+        {
+            return PartialView("PreviousSchool");
+        }
+
+        public PartialViewResult Section()
+        {
+            return PartialView("Section");
+        }
+
+        public PartialViewResult Session()
+        {
+            return PartialView("Session");
+        }
+
+        public PartialViewResult Subject()
+        {
+            return PartialView("Subject");
+        }
+	}
+}
